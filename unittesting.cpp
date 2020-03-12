1c71245bbdc76e13a12119a8bb266c8db18d1aa9
wylieza <wylieza@gmail.com>
2020-03-12 17:49:52 +0200

    Using catch for unit testing and created a file to contain unit tests

---
+++ unittesting.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "HuffmanTree.h"
+#include "HuffmanNode.h"
+
+
+TEST_CASE("Frequency map generation", "[generate_freq_map]"){
+    WYLJUS002::HuffmanTree tree;
+
+}
